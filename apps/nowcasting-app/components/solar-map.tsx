cd9fa36e71a554029979532e1d152b8e5a148981
flowirtz <6052785+flowirtz@users.noreply.github.com>
2022-02-11 15:32:52 +0000

    🔥  Remove unnecessarily complex D3 Layer

--- apps/nowcasting-app/components/solar-map.tsx
+++ apps/nowcasting-app/components/solar-map.tsx
@@ -5,71 +5,9 @@ interface ISolarMap {
 
 import { GeoJsonObject } from "geojson";
 import { GeoJSON } from "react-leaflet";
-import { LatLng, LeafletMouseEvent } from "leaflet";
+import { LeafletMouseEvent } from "leaflet";
 import * as d3 from "d3";
-import * as d3Geo from "d3-geo";
-import {
-  MapContainer,
-  TileLayer,
-  LayerGroup,
-  useMapEvent,
-} from "react-leaflet";
-
-// const D3Layer = ({ geoShape, forecastData }) => {
-//   const map = useMapEvent("zoom", () => {
-//     reset();
-//   });
-
-//   const svg = d3.select(map.getPanes().overlayPane).append("svg");
-//   const g = svg.append("g").attr("class", "leaflet-zoom-hide");
-
-//   // Create a d3.geo.path to convert GeoJSON to SVG
-//   var transform = d3Geo.geoTransform({
-//     point: projectPoint,
-//   });
-//   var path = d3Geo.geoPath().projection(transform);
-//   var features = g
-//     .selectAll("path")
-//     .data(geoShape.features)
-//     .enter()
-//     .append("path");
-
-//   // fit the SVG element to leaflet's map layer
-//   const reset = () => {
-//     const bounds = path.bounds(geoShape);
-//     const topLeft = bounds[0];
-//     const bottomRight = bounds[1];
-
-//     const width = bottomRight[0] - topLeft[0];
-//     const height = bottomRight[1] - topLeft[1];
-
-//     svg
-//       .attr("width", width)
-//       .attr("height", height)
-//       .style("left", topLeft[0] + "px")
-//       .style("top", topLeft[1] + "px");
-
-//     g.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
-
-//     // initialize the path data
-//     features
-//       .attr("d", path)
-//       .attr("class", "stroke-white")
-//       .style("fill", function (d) {
-//         return getForecastColorForGSP(d.properties.gsp_id);
-//       });
-//   };
-
-//   reset();
-
-//   // Use Leaflet to implement a D3 geometric transformation.
-//   function projectPoint(x, y) {
-//     var point = map.latLngToLayerPoint(new LatLng(y, x));
-//     this.stream.point(point.x, point.y);
-//   }
-
-//   return null;
-// };
+import { MapContainer, TileLayer } from "react-leaflet";
 
 const SolarMap = ({ gspregionData, forecastData }: ISolarMap) => {
   // GET THE COLORS FOR THE SHAPES
@@ -133,10 +71,6 @@ const SolarMap = ({ gspregionData, forecastData }: ISolarMap) => {
       scrollWheelZoom={false}
       className="h-[52rem]"
     >
-      {/* <LayerGroup>
-        <D3Layer geoShape={gspregionData} forecastData={forecastData} />
-      </LayerGroup> */}
-
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
