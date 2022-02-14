43ac493bf60e3254c453cb9434132ee481e8a1ae
flowirtz <6052785+flowirtz@users.noreply.github.com>
2022-02-14 11:58:17 +0000

    💄 Display env in navbar

--- apps/nowcasting-app/components/layout.tsx
+++ apps/nowcasting-app/components/layout.tsx
@@ -3,16 +3,23 @@ import Head from "next/head";
 import Footer from "./footer";
 import Navbar from "./navbar";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+interface ILayout {
+  children: React.ReactNode;
+  environment: "local" | "dev";
+}
+
+const Layout = ({ children, environment }: ILayout) => {
   return (
     <>
       <Head>
         <title>Nowcasting App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar />
+      <Navbar environment={environment} />
       <main>{children}</main>
       <Footer />
     </>
   );
-}
+};
+
+export default Layout;
