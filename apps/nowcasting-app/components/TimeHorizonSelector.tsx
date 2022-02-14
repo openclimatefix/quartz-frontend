537f49346ae8c91b68301e96475ed514e4e21d4b
flowirtz <6052785+flowirtz@users.noreply.github.com>
2022-02-14 13:20:07 +0000

    ♻️  Extract ButtonGroup to own component

--- apps/nowcasting-app/components/TimeHorizonSelector.tsx
+++ apps/nowcasting-app/components/TimeHorizonSelector.tsx
@@ -1,4 +1,5 @@
-import { classNames, getTimeFromDate } from "./utils";
+import ButtonGroup from "./button-group";
+import { getTimeFromDate } from "./utils";
 
 interface ITimeHorizonSelector {
   selectedTimeHorizon: number;
@@ -24,55 +25,13 @@ const TimeHorizonSelector = ({
   setSelectedTimeHorizon,
   targetTimes,
 }: ITimeHorizonSelector) => {
-  const ACTIVE_BUTTON_STYLES =
-    "bg-danube-500 focus:ring-black focus:border-black text-white";
-  const INACTIVE_BUTTON_STYLES =
-    "bg-white hover:bg-gray-50 focus:ring-danube-500 focus:border-danube-500";
-  const BASE_BUTTON_STYLES =
-    "relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 border border-gray-300 focus:z-10 focus:outline-none focus:ring-1";
+  const options = [0, 1, 2].map((i) => ({
+    label: getTimeFromDate(new Date(targetTimes[i].targetTime)),
+    active: selectedTimeHorizon === i,
+    onClick: () => setSelectedTimeHorizon(i),
+  }));
 
-  return (
-    <div className="flex justify-center w-full">
-      <span className="relative z-0 inline-flex rounded-md shadow-sm">
-        <button
-          className={classNames(
-            selectedTimeHorizon === 0
-              ? ACTIVE_BUTTON_STYLES
-              : INACTIVE_BUTTON_STYLES,
-            BASE_BUTTON_STYLES,
-            "rounded-l-md"
-          )}
-          onClick={() => setSelectedTimeHorizon(0)}
-        >
-          {getTimeFromDate(new Date(targetTimes[0].targetTime))}
-        </button>
-        <button
-          className={classNames(
-            selectedTimeHorizon === 1
-              ? ACTIVE_BUTTON_STYLES
-              : INACTIVE_BUTTON_STYLES,
-            BASE_BUTTON_STYLES,
-            "-ml-px"
-          )}
-          onClick={() => setSelectedTimeHorizon(1)}
-        >
-          {getTimeFromDate(new Date(targetTimes[1].targetTime))}
-        </button>
-        <button
-          className={classNames(
-            selectedTimeHorizon === 2
-              ? ACTIVE_BUTTON_STYLES
-              : INACTIVE_BUTTON_STYLES,
-            BASE_BUTTON_STYLES,
-            "-ml-px rounded-r-md"
-          )}
-          onClick={() => setSelectedTimeHorizon(2)}
-        >
-          {getTimeFromDate(new Date(targetTimes[2].targetTime))}
-        </button>
-      </span>
-    </div>
-  );
+  return <ButtonGroup options={options} />;
 };
 
 export default TimeHorizonSelector;
---
+++ apps/nowcasting-app/components/button-group.tsx
@@ -0,0 +1,57 @@
+import { classNames } from "./utils";
+
+interface IButtonGroup {
+  options: [
+    {
+      label: string;
+      active: boolean;
+      onClick: React.Dispatch<React.SetStateAction<any>>;
+    }
+  ];
+}
+
+const ButtonGroup = ({ options }: IButtonGroup) => {
+  const ACTIVE_BUTTON_STYLES =
+    "bg-danube-500 focus:ring-black focus:border-black text-white";
+  const INACTIVE_BUTTON_STYLES =
+    "bg-white hover:bg-gray-50 focus:ring-danube-500 focus:border-danube-500";
+  const BASE_BUTTON_STYLES =
+    "relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 border border-gray-300 focus:z-10 focus:outline-none focus:ring-1";
+  const FIRST_BUTTON_STYLES = "rounded-l-md";
+  const LAST_BUTTON_STYLES = "rounded-r-md";
+
+  return (
+    <span className="relative z-0 inline-flex rounded-md shadow-sm">
+      {options.length === 1 && (
+        <button
+          className={classNames(
+            options[0].active ? ACTIVE_BUTTON_STYLES : INACTIVE_BUTTON_STYLES,
+            BASE_BUTTON_STYLES,
+            FIRST_BUTTON_STYLES,
+            LAST_BUTTON_STYLES
+          )}
+          onClick={options[0].onClick}
+        >
+          {options[0].label}
+        </button>
+      )}
+      {options.length >= 2 &&
+        options.map(({ label, onClick, active }, index) => (
+          <button
+            className={classNames(
+              active ? ACTIVE_BUTTON_STYLES : INACTIVE_BUTTON_STYLES,
+              index === 0 && FIRST_BUTTON_STYLES,
+              index > 0 && "-ml-px",
+              index === options.length - 1 && LAST_BUTTON_STYLES,
+              BASE_BUTTON_STYLES
+            )}
+            onClick={onClick}
+          >
+            {label}
+          </button>
+        ))}
+    </span>
+  );
+};
+
+export default ButtonGroup;
