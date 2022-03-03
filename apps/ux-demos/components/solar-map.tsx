0aed77bb3c476433a67ef41d3df7d82b1b100d13
flowirtz <6052785+flowirtz@users.noreply.github.com>
2022-03-03 17:08:55 +0000

    More stuff

--- apps/ux-demos/components/solar-map.tsx
+++ apps/ux-demos/components/solar-map.tsx
@@ -1,6 +1,6 @@
 interface ISolarMap {
   gspregionData: GeoJsonObject;
-  forecastDataGSP: object;
+  // forecastDataGSP: object;
   forecastDataNational: object;
   selectedTimeHorizon: number;
   showGSPForecast: boolean;
@@ -9,7 +9,7 @@ interface ISolarMap {
 // import React, { useRef } from "react";
 
 import { GeoJsonObject } from "geojson";
-import { GeoJSON } from "react-leaflet";
+import { Circle, GeoJSON } from "react-leaflet";
 import { LeafletMouseEvent, GeoJSON as LeafletGeoJSON } from "leaflet";
 import * as d3 from "d3";
 import { MapContainer, TileLayer } from "react-leaflet";
@@ -23,6 +23,7 @@ import {
 import { useEffect, useRef } from "react";
 
 import gspRegionData from "../data/gspRegions.json";
+import forecastDataGSP from "../data/gspError.json";
 
 const SHADES_OF_YELLOW = {
   DEFAULT: "#FFC425",
@@ -40,26 +41,23 @@ const SHADES_OF_YELLOW = {
 
 const SolarMap = ({
   // gspregionData,
-  forecastDataGSP,
+  // forecastDataGSP,
   forecastDataNational,
   selectedTimeHorizon,
+  mapHeight = "calc(100vh - 2rem - 1rem)",
+  circles = false,
 }: ISolarMap) => {
   const getForecastDataByGSPId = (gspId) => forecastDataGSP[gspId - 1];
-  const forecastAccessor =
-    getForecastAccessorForTimeHorizon(selectedTimeHorizon);
   const getForecastColorForGSP = (id) => {
-    const colorScale = d3
-      .scaleQuantize()
-      .domain(d3.extent(forecastDataGSP, forecastAccessor))
-      .range([
-        SHADES_OF_YELLOW["100"],
-        SHADES_OF_YELLOW["300"],
-        SHADES_OF_YELLOW["500"],
-        SHADES_OF_YELLOW["700"],
-        SHADES_OF_YELLOW["900"],
-      ]);
+    const colorScale = d3.scaleQuantize().domain([10, 1000]).range([
+      // SHADES_OF_YELLOW["100"],
+      SHADES_OF_YELLOW["300"],
+      // SHADES_OF_YELLOW["500"],
+      SHADES_OF_YELLOW["700"],
+      // SHADES_OF_YELLOW["900"],
+    ]);
 
-    return colorScale(forecastAccessor(getForecastDataByGSPId(id)));
+    return colorScale(getForecastDataByGSPId(getForecastDataByGSPId(id)));
   };
 
   const generateTableRow = ({
@@ -116,13 +114,31 @@ const SolarMap = ({
 
   const addColorToEachGSP = (feature) => {
     return {
-      // fillColor: getForecastColorForGSP(feature.properties.gsp_id),
-      fillColor: "red",
+      fillColor: getForecastColorForGSP(feature.properties.gsp_id),
+      // fillColor: "red",
       fillOpacity: 1,
       color: "white",
       weight: 1,
     };
   };
+
+  const BBOX = {
+    xMin: -5.181921848720619,
+    xMax: 0.6515741240632,
+    yMin: 50.69113131422758,
+    yMax: 58.77665632199607,
+  };
+
+  // const getRandomGPSCoordsInGB = () => {
+  const randomCoords = [];
+  for (let step = 0; step < 500; step++) {
+    randomCoords.push([
+      Math.random() * (BBOX.yMax - BBOX.yMin + 1) + BBOX.yMin,
+      Math.random() * (BBOX.xMax - BBOX.xMin + 1) + BBOX.xMin,
+    ]);
+  }
+  // };
+  // const  = ;
 
   return (
     <div className="h-full bg-yellow-600">
@@ -130,18 +146,28 @@ const SolarMap = ({
         center={[55.00366, -2.547855]}
         zoom={5.5}
         scrollWheelZoom={false}
-        style={{ height: "calc(100vh - 2rem - 1rem)" }}
+        style={{ height: mapHeight }}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <GeoJSON
-          // ref={geoJsonLayerRef}
-          data={gspRegionData}
-          // onEachFeature={addPopupToEachGSP}
-          style={addColorToEachGSP}
-        />
+        {circles ? (
+          randomCoords.map((coord) => (
+            <Circle
+              center={coord}
+              pathOptions={{ fillColor: "blue" }}
+              radius={10000}
+            />
+          ))
+        ) : (
+          <GeoJSON
+            // ref={geoJsonLayerRef}
+            data={gspRegionData}
+            // onEachFeature={addPopupToEachGSP}
+            style={addColorToEachGSP}
+          />
+        )}
       </MapContainer>
     </div>
   );
