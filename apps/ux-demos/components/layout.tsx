4e1869f1e3b23369dce37a57ea5cd08edb8c2c6a
flowirtz <6052785+flowirtz@users.noreply.github.com>
2022-03-16 11:21:15 +0000

    Add title to Layout

--- apps/ux-demos/components/layout.tsx
+++ apps/ux-demos/components/layout.tsx
@@ -2,9 +2,10 @@ import Head from "next/head";
 
 interface ILayout {
   children: React.ReactNode;
+  title?: string;
 }
 
-const Layout = ({ children }: ILayout) => {
+const Layout = ({ children, title }: ILayout) => {
   return (
     <>
       <Head>
@@ -13,7 +14,7 @@ const Layout = ({ children }: ILayout) => {
       <div className="absolute inset-0 flex flex-col">
         <div className="flex-grow">{children}</div>
         <h1 className="py-2 text-2xl text-center text-white border-t border-white bg-mapbox-black">
-          June 10th, 2021
+          {title && <>{title} | </>}June 10th, 2021
         </h1>
       </div>
     </>
