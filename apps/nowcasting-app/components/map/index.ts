1ca0747c60573746123b07465e1a1f5b62654eba
Alaa Tohamy <alaatohamy4@gmail.com>
2022-05-16 14:52:53 +0200

    style: add end file new line

--- apps/nowcasting-app/components/map/index.ts
+++ apps/nowcasting-app/components/map/index.ts
@@ -2,4 +2,3 @@ export { default as FaildStateMap } from "./faildState";
 export { default as Map } from "./map";
 export { default as LoadStateMap } from "./loadingState";
 export {default as PvLatestMap } from "./pvLatest";
-
