43fb395461330410b3537f5fddc4af068ae138e3
flowirtz <6052785+flowirtz@users.noreply.github.com>
2022-05-30 11:41:21 +0100

    Make the icons centred

--- apps/nowcasting-website/components/users.tsx
+++ apps/nowcasting-website/components/users.tsx
@@ -4,30 +4,35 @@ const users = [
   {
     name: "Grid Operators",
     icon: "electric-pole",
+    className: "",
   },
   {
     name: "Solar Farms",
     icon: "solar-panels",
+    className: "",
   },
   {
     name: "Electricity Traders",
     icon: "bolt",
+    className: "",
   },
   {
     name: "Battery Operators",
     icon: "lighting",
+    className: "col-start-2",
   },
   {
     name: "Smart Homes",
     icon: "home-automation",
+    className: "",
   },
 ];
 
 const Users = ({}: IUsers) => {
   return (
-    <ul className="grid grid-cols-3 gap-10">
-      {users.map(({ name, icon }) => (
-        <li key={name}>
+    <ul className="grid grid-cols-6 gap-10">
+      {users.map(({ name, icon, className }) => (
+        <li key={name} className={`col-span-2 ${className}`}>
           <figure>
             <img
               className="w-20 h-20 mx-auto"
