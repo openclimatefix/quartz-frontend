8af85e73fe8f3cd288ff33c4cd9200c81c9733b1
lina <linah.ahmed.92@gmail.com>
2022-06-20 00:28:30 +0200

    add error toast

--- apps/nowcasting-app/pages/_app.tsx
+++ apps/nowcasting-app/pages/_app.tsx
@@ -1,13 +1,26 @@
 import type { AppProps } from "next/app";
 import { UserProvider } from "@auth0/nextjs-auth0";
-
+import { ToastContainer, toast } from "react-toastify";
 import "../styles/globals.css";
 import { SWRConfig } from "swr";
 
 function MyApp({ Component, pageProps }: any) {
   return (
     <UserProvider>
-      <SWRConfig value={{ provider: () => new Map() }}>
+      <SWRConfig
+        value={{
+          provider: () => new Map(),
+          onError: (error, key) => {
+            toast("Error fetching data. Retrying now…", {
+              type: "error",
+              theme: "colored",
+              icon: false,
+            });
+          },
+        }}
+      >
+        <ToastContainer position="top-left" autoClose={10000} limit={5} hideProgressBar={true} />
+
         <Component {...pageProps} />
       </SWRConfig>
     </UserProvider>
