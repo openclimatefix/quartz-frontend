f35372da06022edc4be1ba24c9f1c6169ebdc3f7
AlaaTohamy <alaatohamy4@gmail.com>
2022-06-19 15:39:54 +0200

    Fix/loading state (#98)
    * fix: loading state in the line chart
    
    * fix: overflow in line chart section
    
    * Add background gray while loading map
    
    * fix: merging development branch

--- apps/nowcasting-app/components/charts/gsp-pv-remix-chart/index.tsx
+++ apps/nowcasting-app/components/charts/gsp-pv-remix-chart/index.tsx
@@ -25,8 +25,8 @@ const GspPvRemixChart: FC<{
   if (errors.length) return <div>failed to load</div>;
   if (!fcAll || !pvRealDataIn || !pvRealDataAfter)
     return (
-      <div className="m-auto w-full h-60 flex">
-        <Spinner></Spinner>
+      <div className="h-60 flex">
+        <Spinner />
       </div>
     );
   const forcastAtSelectedTime: NonNullable<typeof gspForecastData>[number] =
--- apps/nowcasting-app/components/map/map.tsx
+++ apps/nowcasting-app/components/map/map.tsx
@@ -60,7 +60,7 @@ const Map = ({ loadDataOverlay, controlOverlay, bearing = 0, updateData }: IMap)
   }, [map]);
 
   return (
-    <div className="relative h-full">
+    <div className="relative h-full bg-mapbox-black-500">
       <div className="absolute top-0 left-0 z-10 px-2 py-3 m-3 min-w-[20rem] w-full">
         {controlOverlay(map)}
       </div>
