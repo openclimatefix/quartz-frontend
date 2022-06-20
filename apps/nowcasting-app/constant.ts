00ab85821fd450d3404cde5f081d9c28848671e9
lina <linah.ahmed.92@gmail.com>
2022-06-20 13:25:44 +0200

    add spicific error msg

--- apps/nowcasting-app/constant.ts
+++ apps/nowcasting-app/constant.ts
@@ -6,3 +6,36 @@ export const getAllForecastUrl = (isNormalized: boolean, isHistoric: boolean) =>
   `${API_PREFIX}/GB/solar/gsp/forecast/all?${isHistoric ? "historic=true" : ""}${
     isNormalized ? "&normalize=true" : ""
   }`;
+
+export const apiErrorMSGS = [
+  {
+    key: /\/GB\/solar\/gsp\/forecast\/all\??$/,
+    msg: "Error fetching initial data. Retrying now…",
+  },
+  {
+    key: /\/GB\/solar\/gsp\/forecast\/all\?historic=true&normalize=true$/,
+    msg: "Error fetching map data. Retrying now…",
+  },
+
+  {
+    key: /\/GB\/solar\/gsp\/forecast\/latest\/0$/,
+    msg: "Error fetching national forcasts data. Retrying now…",
+  },
+  {
+    key: /\/GB\/solar\/gsp\/truth\/one_gsp\/0\/\?regime=in-day$/,
+    msg: 'Error fetching "NA-INDAY" data. Retrying now…',
+  },
+  {
+    key: /\/GB\/solar\/gsp\/truth\/one_gsp\/0\/\?regime=day-after$/,
+    msg: 'Error fetching "NA-DAYAFTER" data. Retrying now…',
+  },
+
+  {
+    key: /\/GB\/solar\/gsp\/truth\/one_gsp\/\d+\/\?regime=in-day$/,
+    msg: 'Error fetching "GSP-INDAY" data. Retrying now…',
+  },
+  {
+    key: /\/GB\/solar\/gsp\/truth\/one_gsp\/\d+\/\?regime=day-after$/,
+    msg: 'Error fetching "GSP-DAYAFTER" data. Retrying now…',
+  },
+];
--- apps/nowcasting-app/pages/_app.tsx
+++ apps/nowcasting-app/pages/_app.tsx
@@ -3,6 +3,7 @@ import { UserProvider } from "@auth0/nextjs-auth0";
 import { ToastContainer, toast } from "react-toastify";
 import "../styles/globals.css";
 import { SWRConfig } from "swr";
+import { apiErrorMSGS } from "../constant";
 
 function MyApp({ Component, pageProps }: any) {
   return (
@@ -11,7 +12,9 @@ function MyApp({ Component, pageProps }: any) {
         value={{
           provider: () => new Map(),
           onError: (error, key) => {
-            toast("Error fetching data. Retrying now…", {
+            const defaultMsg = "Error fetching data. Retrying now…";
+            const errorMsg = apiErrorMSGS.find((e) => key.match(e.key))?.msg;
+            toast(errorMsg || defaultMsg, {
               type: "error",
               theme: "colored",
               icon: false,
