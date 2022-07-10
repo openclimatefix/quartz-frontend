2a57c4b8e079b1ade9bba1cf54dc664e141d7788
lina <linah.ahmed.92@gmail.com>
2022-07-10 11:55:28 +0200

    complete dummy api data

--- apps/nowcasting-app/next.config.js
+++ apps/nowcasting-app/next.config.js
@@ -7,6 +7,7 @@ const { withSentryConfig } = require("@sentry/nextjs");
 
 const moduleExports = {
   // Your existing module.exports
+  trailingSlash: true,
 };
 
 const sentryWebpackPluginOptions = {
--- apps/nowcasting-app/pages/api/forecasts/[...mockApiRoute].ts
+++ apps/nowcasting-app/pages/api/forecasts/[...mockApiRoute].ts
@@ -4,9 +4,13 @@ import { withSentry } from "@sentry/nextjs";
 import gbPvGSPJson from "../../../data/dummy-res/fc-gsp.json";
 import gspRegions from "../../../data/dummy-res/gsp-regions.json";
 import forecastNat from "../../../data/dummy-res/forecast-national.json";
-import truthAll from "../../../data/dummy-res/truth-all.json";
+import truthAllDayIn from "../../../data/dummy-res/truth-all-day-in.json";
+import truthAllDayAfter from "../../../data/dummy-res/truth-all-day-after.json";
+import gspDayIn from "../../../data/dummy-res/gsp-day-in.json";
+import gspDayAfter from "../../../data/dummy-res/gsp-day-after.json";
 import fc_0 from "../../../data/dummy-res/fc-latest-0.json";
 import fc_all from "../../../data/dummy-res/fc-all.json";
+import solar_status from "../../../data/dummy-res/solar-status.json";
 
 function handler(req: NextApiRequest, res: NextApiResponse) {
   const { mockApiRoute } = req.query;
@@ -17,12 +21,20 @@ function handler(req: NextApiRequest, res: NextApiResponse) {
     res.status(200).json(gspRegions);
   } else if ((mockApiRoute as string[]).join("/") === "GB/solar/gsp/forecast/national") {
     res.status(200).json(forecastNat);
-  } else if ((mockApiRoute as string[]).join("/").startsWith("GB/solar/gsp/pvlive/one_gsp")) {
-    res.status(200).json(truthAll);
+  } else if ((mockApiRoute as string[]).join("/") === "GB/solar/gsp/pvlive/one_gsp/0") {
+    if (req.query.regime === "in-day") {
+      res.status(200).json(truthAllDayIn);
+    } else res.status(200).json(truthAllDayAfter);
+  } else if ((mockApiRoute as string[]).join("/").match(/GB\/solar\/gsp\/pvlive\/one_gsp\/\d+/)) {
+    if (req.query.regime === "in-day") {
+      res.status(200).json(gspDayIn);
+    } else res.status(200).json(gspDayAfter);
   } else if ((mockApiRoute as string[]).join("/") === "GB/solar/gsp/forecast/latest/0") {
     res.status(200).json(fc_0);
   } else if ((mockApiRoute as string[]).join("/") === "GB/solar/gsp/forecast/all") {
     res.status(200).json(fc_all);
+  } else if ((mockApiRoute as string[]).join("/") === "GB/solar/status") {
+    res.status(200).json(solar_status);
   } else {
     res.status(400).json({ type: "error", message: "Bad request" });
   }
