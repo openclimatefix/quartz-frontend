5909e8e2386fee6498b53837d4a887e6b6198ef8
lina <linah.ahmed.92@gmail.com>
2022-07-20 12:59:52 +0200

    create a sepreate dummy api for testing

--- apps/nowcasting-app/constant.ts
+++ apps/nowcasting-app/constant.ts
@@ -1,6 +1,6 @@
 export const API_PREFIX =
   process.env.NEXT_PUBLIC_ENV_NAME === "test"
-    ? "/api/forecasts"
+    ? "/api/forecasts-test"
     : "https://api-dev.nowcasting.io/v0";
 export const MAX_POWER_GENERATED = 500;
 export const MAX_NATIONAL_GENERATION_MW = 12000;
