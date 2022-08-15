d017caba5600f5481f266690514912e2fedcbe61
lina <linah.ahmed.92@gmail.com>
2022-08-15 09:45:28 +0200

    setup jest

---
+++ apps/nowcasting-app/.babelrc.jest.js
@@ -0,0 +1,14 @@
+// Necessary to make Jest work with TypeScript
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "current",
+        },
+      },
+    ],
+    "@babel/preset-typescript",
+  ],
+};
