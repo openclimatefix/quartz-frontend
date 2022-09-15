5b156ddc1a7a70fa0300bc92a67b69384124b358
rachel-labri-tipton <rachemtipton@gmail.com>
2022-09-15 17:12:03 +0200

    initial attempt at adding mw and gw to titles

--- apps/nowcasting-app/components/charts/gsp-pv-remix-chart/forecast-header-gsp.tsx
+++ apps/nowcasting-app/components/charts/gsp-pv-remix-chart/forecast-header-gsp.tsx
@@ -8,7 +8,7 @@ type ForecastHeaderGSPProps = {
 const ForecastHeaderGSP: React.FC<ForecastHeaderGSPProps> = ({ title, children, onClose }) => {
   return (
     <div id="x" className={"flex content-between flex-wrap mt-6 bg-ocf-yellow bg-opacity-60 h-12"}>
-      <div className={`bg-white text-black text-2xl font-black  p-4 py-2  flex-[1]`}>{title}</div>
+      <div className={`bg-white text-black text-2xl font-black  p-4 py-2  flex-[1]`}>{title}<span className={`text-base text-ocf-gray-900 ml-5`}>MW</span></div>
       <div className="flex-[1] m-auto">
         <p className="text-lg text-center align-middle m-auto mx-2">{children}</p>
       </div>
