3b21c60382215c4a24cd53ef088df3087cbdb267
rachel-labri-tipton <rachemtipton@gmail.com>
2022-09-23 17:10:51 +0200

    added X symbol from google icons and updated the close button for GSP forecast header

--- apps/nowcasting-app/pages/_document.tsx
+++ apps/nowcasting-app/pages/_document.tsx
@@ -10,6 +10,7 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
           <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"/>
           <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" defer></script>
         </Head>
         <body className="h-full">
