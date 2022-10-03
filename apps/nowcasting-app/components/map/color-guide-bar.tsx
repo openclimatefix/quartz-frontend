e6466836ca193ea1dc72b98cba8e6e6420f43b72
braddf <bradbdf@gmail.com>
2022-10-03 11:19:58 +0200

    #258: light legend labels to white for contrast

--- apps/nowcasting-app/components/map/color-guide-bar.tsx
+++ apps/nowcasting-app/components/map/color-guide-bar.tsx
@@ -7,19 +7,21 @@ const ColorGuideBar: React.FC<ColorGuideBarProps> = ({ unit }) => {
   const values = useMemo(() => {
     if (unit === ActiveUnit.MW) {
       return [
-        { value: "50-150", opacity: 20 },
-        { value: "150-250", opacity: 40 },
-        { value: "250-350", opacity: 60 },
-        { value: "350-450", opacity: 80 },
-        { value: "450+", opacity: 100 }
+        { value: "0-50", opacity: 10, textColor: "ocf-gray-300" },
+        { value: "50-150", opacity: 20, textColor: "ocf-gray-300" },
+        { value: "150-250", opacity: 40, textColor: "ocf-gray-300" },
+        { value: "250-350", opacity: 60, textColor: "black" },
+        { value: "350-450", opacity: 80, textColor: "black" },
+        { value: "450+", opacity: 100, textColor: "black" }
       ];
     } else {
       return [
-        { value: "10-30", opacity: 20 },
-        { value: "30-50", opacity: 40 },
-        { value: "50-70", opacity: 60 },
-        { value: "70-90", opacity: 80 },
-        { value: "90+", opacity: 100 }
+        { value: "0-10", opacity: 10, textColor: "ocf-gray-300" },
+        { value: "10-30", opacity: 20, textColor: "ocf-gray-300" },
+        { value: "30-50", opacity: 40, textColor: "ocf-gray-300" },
+        { value: "50-70", opacity: 60, textColor: "black" },
+        { value: "70-90", opacity: 80, textColor: "black" },
+        { value: "90+", opacity: 100, textColor: "black" }
       ];
     }
   }, [unit]);
@@ -32,11 +34,17 @@ const ColorGuideBar: React.FC<ColorGuideBarProps> = ({ unit }) => {
             key={value.value}
             className={`px-3 py-[1px] bg-ocf-yellow/${value.opacity} whitespace-nowrap ${
               index !== 0 ? "border-l border-ocf-black-600" : ""
-            }`}
+            } text-${value.textColor}`}
           >
             {value.value}
             {index === 0 && (
-              <span className="font-normal text-ocf-black-500 text-xs ml-1">{unitText}</span>
+              <span
+                className={`font-normal ${
+                  value.textColor === "black" ? "text-ocf-black-500" : "text-ocf-gray-600"
+                } text-xs ml-1`}
+              >
+                {unitText}
+              </span>
             )}
           </div>
         ))}
