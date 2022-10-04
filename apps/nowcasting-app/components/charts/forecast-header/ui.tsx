4dcd8668147de54ce723f7542dfe295898f131aa
rachel-labri-tipton <rachemtipton@gmail.com>
2022-10-04 10:03:36 +0200

    adding tooltip to forecast readings

--- apps/nowcasting-app/components/charts/forecast-header/ui.tsx
+++ apps/nowcasting-app/components/charts/forecast-header/ui.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { theme } from "../../../tailwind.config";
+import Tooltip from "../../tooltip";
 const yellow = theme.extend.colors["ocf-yellow"].DEFAULT;
 
-const PVNumber: React.FC<{ pv: string; title: string; color?: string }> = ({
+const PVNumber: React.FC<{ pv: string; title: string; tooltip: string; color?: string }> = ({
   pv,
   title,
+  tooltip,
   color = yellow
 }) => {
   return (
     <div className="flex-[1] m-auto bg-ocf-gray-900">
       <div className="">
-        <div className="flex justify-center mt-1">
+        <div className="flex justify-start">
           <svg
-            viewBox="0 0 32 24"
-            width="1.5rem"
+            viewBox="0 0 48 48"
+            width="2rem"
             height="2rem"
             xmlns="http://www.w3.org/2000/svg"
             fill-rule="evenodd"
@@ -25,15 +27,25 @@ const PVNumber: React.FC<{ pv: string; title: string; color?: string }> = ({
              0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm0 11h6v1h-7v-9h1v8z"
             />
           </svg>
-          <p className="text-white whitespace-pre text-center mt-2 text-sm">{title}</p>
+          <p className="text-white whitespace-pre text-center mt-1 text-sm">{title}</p>
         </div>
-        <p
-          className={`lg:text-xl md:text-lg text-sm font-bold text-center text-${color}`}
-          style={{ color: color }}
+        <Tooltip
+          className="bg-ocf-gray-900"
+          tip={
+            <div className="bg-ocf-gray-900 bg-opacity-50">
+              <p>{tooltip}</p>
+            </div>
+          }
+          position="right"
         >
-          {pv}
-          <span className=" ml-2 text-white">GW</span>
-        </p>
+          <p
+            className={`lg:text-xl md:text-lg text-sm font-bold text-center text-${color}`}
+            style={{ color: color }}
+          >
+            {pv}
+            <span className="ml-2 text-white">GW</span>
+          </p>
+        </Tooltip>
       </div>
     </div>
   );
@@ -57,15 +69,19 @@ const ForecastHeaderUI: React.FC<ForecastHeaderProps> = ({
   forecastNextTimeOnly
 }) => {
   return (
-    <div className={"flex content-between flex-wrap mt-0 h-auto"}>
+    <div className={"grow flex bg-ocf-gray-900 content-between flex-wrap mt-0 h-auto"}>
       <div
-        className={`bg-ocf-gray-900 text-white lg:text-2xl md:text-lg text-sm font-OCF-gray-300 p-4 py-2 flex-[2]`}
+        className={`bg-ocf-gray-900 text-white lg:text-2xl md:text-lg text-sm font-OCF-gray-300`}
       >
         National
       </div>
-      <PVNumber pv={actualPV} title={`${pvTimeOnly} PVLive`} color="black" />
-      <PVNumber pv={forcastPV} title={`${selectedTimeOnly} Forecast`} />
-      <PVNumber pv={forcastNextPV} title={`${forecastNextTimeOnly} Forecast`} />
+      <PVNumber pv={actualPV} title={`${pvTimeOnly}`} tooltip={`PV Actual`} color="black" />
+      <PVNumber pv={forcastPV} title={`${selectedTimeOnly}`} tooltip={`OCF Forecast`} />
+      <PVNumber
+        pv={forcastNextPV}
+        title={`${forecastNextTimeOnly}`}
+        tooltip={`Next OCF Forecast`}
+      />
       <div className="inline-flex bg-ocf-gray-900 items-center h-full py-2">{children}</div>
     </div>
   );
