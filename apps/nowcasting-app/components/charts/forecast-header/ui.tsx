50f6e91737d8d6f5209a3865959f1bfda3a3fb69
rachel-labri-tipton <rachemtipton@gmail.com>
2022-10-09 21:44:55 +0200

    setting new layout for gsp header

--- apps/nowcasting-app/components/charts/forecast-header/ui.tsx
+++ apps/nowcasting-app/components/charts/forecast-header/ui.tsx
@@ -2,22 +2,38 @@ import React from "react";
 import { theme } from "../../../tailwind.config";
 const yellow = theme.extend.colors["ocf-yellow"].DEFAULT;
 
-const PVNumber: React.FC<{ pv: string; subTitle: string; color?: string }> = ({
+const PVNumber: React.FC<{ pv: string; time: string; color?: string }> = ({
   pv,
-  subTitle,
+  time,
   color = yellow
 }) => {
   return (
     <div className="flex-[1] m-auto">
       <div className="">
+        <p className="">
+          <svg
+            viewBox="0 0 48 48"
+            width="2rem"
+            height="2rem"
+            xmlns="http://www.w3.org/2000/svg"
+            fill-rule="evenodd"
+            fill="white"
+            clip-rule="evenodd"
+          >
+            <path
+              d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071
+             0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm0 11h6v1h-7v-9h1v8z"
+            />
+          </svg>
+          {time}
+        </p>
         <p
-          className={`lg:text-xl md:text-lg text-sm font-bold text-center text-${color}`}
+          className={`lg:text-xl md:text-lg text-sm font-semibold text-center text-${color}`}
           style={{ color: color }}
         >
           {pv}
-          <span className=" ml-2 text-white">GW</span>
+          <span className="text-xs text-ocf-gray-300 font-normal">GW</span>
         </p>
-        <p className="text-white whitespace-pre text-center ">{subTitle}</p>
       </div>
     </div>
   );
@@ -41,16 +57,24 @@ const ForecastHeaderUI: React.FC<ForecastHeaderProps> = ({
   forecastNextTimeOnly
 }) => {
   return (
-    <div className={"flex content-between flex-wrap mt-6 h-auto"}>
-      <div
-        className={`bg-white text-black lg:text-2xl md:text-lg text-sm font-black p-4 py-2 flex-[2]`}
-      >
-        National Solar PV <span className={`text-base text-ocf-gray-900 ml-2`}>MW</span>
+    <div className={"flex content-between bg-ocf-gray-800 flex-wrap h-auto"}>
+      <div className={`text-white lg:text-2xl md:text-lg text-sm font-black m-auto ml-5 flex-[2]`}>
+        National
       </div>
-      <PVNumber pv={actualPV} subTitle={`${pvTimeOnly} PVLive`} color="black" />
-      <PVNumber pv={forcastPV} subTitle={`${selectedTimeOnly} Forecast`} />
-      <PVNumber pv={forcastNextPV} subTitle={`${forecastNextTimeOnly} Forecast`} />
-      <div className=" inline-flex items-center h-full m-auto">{children}</div>
+      <PVNumber
+        pv={
+          <div>
+            <span className="text-ocf-yellow font-semibold">{forcastPV}</span>
+            <span className="text-white font-semibold">/</span>
+            {actualPV}
+          </div>
+        }
+        time={`${pvTimeOnly}`}
+        color="black"
+      />
+      {/* <PVNumber pv={forcastPV} time={`${selectedTimeOnly}`} color="ocf-yellow" /> */}
+      <PVNumber pv={forcastNextPV} time={`${forecastNextTimeOnly}`} color="ocf-yellow" />
+      <div className="inline-flex items-center h-full">{children}</div>
     </div>
   );
 };
