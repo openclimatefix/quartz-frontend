7c5561e1f11cfb20086444020b46f692fcab3ad5
rachel-labri-tipton <rachemtipton@gmail.com>
2022-10-13 18:44:18 +0200

    adjusting color for national forecast header numbers

--- apps/nowcasting-app/components/charts/forecast-header/ui.tsx
+++ apps/nowcasting-app/components/charts/forecast-header/ui.tsx
@@ -1,8 +1,33 @@
 import React from "react";
 import { theme } from "../../../tailwind.config";
+import { ClockIcon } from "../../icons";
 const yellow = theme.extend.colors["ocf-yellow"].DEFAULT;
 
-const PVNumber: React.FC<{ pv: string; time: string; color?: string }> = ({
+const ForecastWithActualPV: React.FC<{
+  forecast: string;
+  pv: string;
+  time: string;
+  color?: string;
+}> = ({ forecast, pv, time, color = yellow }) => {
+  return (
+    <div className="flex flex-col m-auto h-10">
+      <div className="flex justify-items-start">
+        <ClockIcon />
+        <p className="text-xs">{time}</p>
+      </div>
+      <div>
+        <p className={`text-lg font-semibold text-center text-${color}`} style={{ color: color }}>
+          {forecast}
+          <span className="text-ocf-gray-300">/</span>
+          <span className="text-black">{pv}</span>
+          <span className="text-xs text-ocf-gray-300 font-normal"> GW</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const NextForecast: React.FC<{ pv: string; time: string; color?: string }> = ({
   pv,
   time,
   color = yellow
@@ -10,20 +35,7 @@ const PVNumber: React.FC<{ pv: string; time: string; color?: string }> = ({
   return (
     <div className="flex flex-col m-auto h-10">
       <div className="flex justify-items-start">
-        <svg
-          viewBox="0 0 32 32"
-          width="16"
-          height="16"
-          xmlns="http://www.w3.org/2000/svg"
-          fill-rule="evenodd"
-          fill="white"
-          clip-rule="evenodd"
-        >
-          <path
-            d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071
-             0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm0 11h6v1h-7v-9h1v8z"
-          />
-        </svg>
+        <ClockIcon />
         <p className="text-xs">{time}</p>
       </div>
       <div>
@@ -60,10 +72,15 @@ const ForecastHeaderUI: React.FC<ForecastHeaderProps> = ({
       </div>
       <div className="flex justify-between flex-2 mt-1 px-5">
         <div className="pr-10">
-          <PVNumber pv={`${forcastPV}/${actualPV}`} time={`${pvTimeOnly}`} color="black" />
+          <ForecastWithActualPV
+            forecast={`${forcastPV}`}
+            pv={`${actualPV}`}
+            time={`${pvTimeOnly}`}
+            color="ocf-yellow"
+          />
         </div>
         <div>
-          <PVNumber pv={forcastNextPV} time={`${forecastNextTimeOnly}`} color="ocf-yellow" />
+          <NextForecast pv={forcastNextPV} time={`${forecastNextTimeOnly}`} color="ocf-yellow" />
         </div>
       </div>
       <div className="inline-flex h-full">{children}</div>
--- apps/nowcasting-app/components/icons.tsx
+++ apps/nowcasting-app/components/icons.tsx
@@ -9,6 +9,10 @@ type CloseButtonIconProps = {
   className?: string;
 };
 
+type ClockIconProps = {
+  className?: string;
+};
+
 export const LegendLineGraphIcon: React.FC<LegendLineGraphIconProps> = ({
   className,
   dashed = false
@@ -42,6 +46,23 @@ export const CloseButtonIcon: React.FC<CloseButtonIconProps> = ({ className }) =
       strokeWidth={0.5}
       d="M20.030 5.030l-1.061-1.061-6.97 6.97-6.97-6.97-1.061 1.061 6.97 6.97-6.97 6.97 1.061 1.061 6.97-6.97 6.97 6.97 1.061-1.061-6.97-6.97 6.97-6.97z"
       fill="white"
+    />
+  </svg>
+);
+
+export const ClockIcon: React.FC<ClockIconProps> = ({ className }) => (
+  <svg
+    viewBox="0 0 32 32"
+    width="16"
+    height="16"
+    xmlns="http://www.w3.org/2000/svg"
+    fill-rule="evenodd"
+    fill="white"
+    clip-rule="evenodd"
+  >
+    <path
+      d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071
+      0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm0 11h6v1h-7v-9h1v8z"
     />
   </svg>
 );
