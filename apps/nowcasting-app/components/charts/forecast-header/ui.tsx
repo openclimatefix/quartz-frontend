e2874e6b3ee805e29f5afe4524de5f42b3310f4a
rachel-labri-tipton <rachemtipton@gmail.com>
2022-10-13 14:31:32 +0200

    changing forecast display

--- apps/nowcasting-app/components/charts/forecast-header/ui.tsx
+++ apps/nowcasting-app/components/charts/forecast-header/ui.tsx
@@ -8,25 +8,25 @@ const PVNumber: React.FC<{ pv: string; time: string; color?: string }> = ({
   color = yellow
 }) => {
   return (
-    <div className="flex-[1] m-auto">
-      <div className="">
-        <p className="">
-          <svg
-            viewBox="0 0 48 48"
-            width="2rem"
-            height="2rem"
-            xmlns="http://www.w3.org/2000/svg"
-            fill-rule="evenodd"
-            fill="white"
-            clip-rule="evenodd"
-          >
-            <path
-              d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071
+    <div className="flex flex-col m-auto">
+      <div className="flex justify-items-start">
+        <svg
+          viewBox="0 0 32 32"
+          width="16"
+          height="16"
+          xmlns="http://www.w3.org/2000/svg"
+          fill-rule="evenodd"
+          fill="white"
+          clip-rule="evenodd"
+        >
+          <path
+            d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071
              0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm0 11h6v1h-7v-9h1v8z"
-            />
-          </svg>
-          {time}
-        </p>
+          />
+        </svg>
+        <p className="text-xs">{time}</p>
+      </div>
+      <div>
         <p
           className={`lg:text-xl md:text-lg text-sm font-semibold text-center text-${color}`}
           style={{ color: color }}
@@ -57,25 +57,18 @@ const ForecastHeaderUI: React.FC<ForecastHeaderProps> = ({
   forecastNextTimeOnly
 }) => {
   return (
-    <div className={"flex content-between bg-ocf-gray-800 flex-wrap h-auto"}>
-      <div className={`text-white lg:text-2xl md:text-lg text-sm font-black m-auto ml-5 flex-[2]`}>
+    <>
+      <div className="text-white lg:text-2xl md:text-lg text-sm font-black m-auto ml-5 flex justify-evenly">
         National
       </div>
-      <PVNumber
-        pv={
-          <div>
-            <span className="text-ocf-yellow font-semibold">{forcastPV}</span>
-            <span className="text-white font-semibold">/</span>
-            {actualPV}
-          </div>
-        }
-        time={`${pvTimeOnly}`}
-        color="black"
-      />
-      {/* <PVNumber pv={forcastPV} time={`${selectedTimeOnly}`} color="ocf-yellow" /> */}
-      <PVNumber pv={forcastNextPV} time={`${forecastNextTimeOnly}`} color="ocf-yellow" />
-      <div className="inline-flex items-center h-full">{children}</div>
-    </div>
+      <div>
+        <PVNumber pv={`${forcastPV}/${actualPV}`} time={`${pvTimeOnly}`} color="black" />
+      </div>
+      <div>
+        <PVNumber pv={forcastNextPV} time={`${forecastNextTimeOnly}`} color="ocf-yellow" />
+      </div>
+      <div className="inline-flex h-full">{children}</div>
+    </>
   );
 };
 
