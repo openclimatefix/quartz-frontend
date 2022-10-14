59b7e56f73cdaf4be104500c489b61930a194a8e
rachel-labri-tipton <rachemtipton@gmail.com>
2022-10-14 18:02:24 +0200

    setting up pv remix chart legend with now OCF 4hr forecast

--- apps/nowcasting-app/components/charts/pv-remix-chart.tsx
+++ apps/nowcasting-app/components/charts/pv-remix-chart.tsx
@@ -118,12 +118,18 @@ const PvRemixChart: FC<{ date?: string }> = () => {
           ></GspPvRemixChart>
         )}
       </div>
-      <div className="flex-0 px-3 text-[12px] tracking-wider text-ocf-gray-300 py-2 bg-mapbox-black-500">
-        <div className="flex justify-around">
+      <div className="flex flex-row justify-around px-3 text-xs tracking-wider text-ocf-gray-300 py-2 bg-mapbox-black-500">
+        <div className="flex-3 flex-col">
           <LegendItem iconClasses={"text-ocf-black"} dashed label={"PV live initial estimate"} />
           <LegendItem iconClasses={"text-ocf-black"} label={"PV live updated"} />
+        </div>
+        <div className="flex-3 flex-col">
           <LegendItem iconClasses={"text-ocf-yellow"} dashed label={"OCF Forecast"} />
           <LegendItem iconClasses={"text-ocf-yellow"} label={"OCF Final Forecast"} />
+        </div>
+        <div className="flex-3 flex-col">
+          <LegendItem iconClasses={"text-ocf-orange"} dashed label={"OCF 4hr Forecast"} />
+          <LegendItem iconClasses={"text-ocf-orange"} label={"OCF 4hr Final Forecast"} />
         </div>
       </div>
     </div>
