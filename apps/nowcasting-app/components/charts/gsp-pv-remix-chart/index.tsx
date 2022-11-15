142e5c882645f0f5d54f74b0dd8c913760368373
rachel-labri-tipton <rachemtipton@gmail.com>
2022-11-15 12:13:02 +0100

    commenting out 4hforecast code so it can be moved to a separate issue branch

--- apps/nowcasting-app/components/charts/gsp-pv-remix-chart/index.tsx
+++ apps/nowcasting-app/components/charts/gsp-pv-remix-chart/index.tsx
@@ -23,13 +23,14 @@ const GspPvRemixChart: FC<{
   resetTime: () => void;
   visibleLines: string[];
 }> = ({ gspId, selectedTime, close, setTimeOfInterest, timeNow, resetTime, visibleLines }) => {
-  const { errors, fcAll, pvRealDataAfter, pvRealDataIn, gsp4HourData } = useGetGspData(gspId);
+  // add gsp4HourData back in here when working on global state variable for show4hView
+  const { errors, fcAll, pvRealDataAfter, pvRealDataIn } = useGetGspData(gspId);
   const gspData = fcAll?.forecasts.find((fc) => fc.location.gspId === gspId);
   const gspForecastData = gspData?.forecastValues;
   const gspInfo = gspData?.location;
   const chartData = useFormatChartData({
     forecastData: gspForecastData,
-    fourHourData: gsp4HourData,
+    // fourHourData: gsp4HourData,
     pvRealDayInData: pvRealDataIn,
     pvRealDayAfterData: pvRealDataAfter,
     timeTrigger: selectedTime
