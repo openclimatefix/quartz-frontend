06713af93f08699110c45d7b39236761adcc6126
rachel-labri-tipton <rachemtipton@gmail.com>
2023-01-23 14:58:51 +0100

    fixing progress bar hover styling issue

--- apps/nowcasting-app/components/charts/delta-view/delta-view-chart.tsx
+++ apps/nowcasting-app/components/charts/delta-view/delta-view-chart.tsx
@@ -26,6 +26,7 @@ import {
 } from "../../types";
 import Tooltip from "../../tooltip";
 import { ChartInfo } from "../../../ChartInfo";
+import DeltaForecastLabel from "../../delta-forecast-label"
 import { theme } from "../../../tailwind.config";
 
 type DeltaBucketProps = {
@@ -307,6 +308,7 @@ const GspDeltaColumn: FC<{
 }> = ({ gspDeltas, setClickedGspId, negative = false }) => {
   const [selectedBuckets] = useGlobalState("selectedBuckets");
   const [selectedDeltaGsp, setSelectedDeltaGsp] = useGlobalState("selectedDeltaGsp");
+  const [clickedGspId] = useGlobalState("clickedGspId")
   if (!gspDeltas.size) return null;
 
   const sortFunc = (a: GspDeltaValue, b: GspDeltaValue) => {
@@ -386,96 +388,123 @@ const GspDeltaColumn: FC<{
             Number(gspDelta.deltaNormalized) * 100
           ).toFixed(0);
 
-          const tickerColor = `h-2 ${gspDelta.delta > 0 ? `bg-ocf-delta-900` : `bg-ocf-delta-100`}`;
+          const tickerColor = `${selectedDeltaGsp === gspDelta.gspId?  `h-2.5` : `h-2`} ${gspDelta.delta > 0 ? `bg-ocf-delta-900` : `bg-ocf-delta-100`}`;
 
-          const selectedClasses = `flex flex-3 flex-row bg-ocf-delta-1000 pb-0 justify-between pl-1 pr-1 ${
+          const selectedClasses = `static flex flex-3 flex-row pb-0 justify-between pl-1 pr-1 ${
             gspDelta.delta > 0 ? `border-l-8` : `border-r-8`
-          } ${bucketColor} cursor-pointer`;
+          } ${bucketColor} cursor-pointer transition duration-200 ease-out hover:ease-in items-end`;
 
-          const selectedDeltaClass = `flex flex-3 flex-row bg-ocf-gray-800 pb-0 justify-between pl-1 pr-1 ${
+          const selectedDeltaClass = `static flex flex-3 flex-row bg-ocf-gray-800 pb-0 justify-between pl-1 pr-1 ${
             gspDelta.delta > 0 ? `border-l-8` : `border-r-8`
-          } ${bucketColor} cursor-pointer pb-0`;
+          } ${bucketColor} cursor-pointer pb-0 items-end`;
 
           if (!isSelected) {
             return null;
           }
           return (
-            <div className={`pb-0.5`} key={`gspCol${gspDelta.gspId}`}>
+            <>
+              <div className="pb-0.5">
+              <div className={`bg-ocf-delta-1000 transition duration-200 ease-out hover:bg-ocf-gray-700 hover:ease-in`} key={`gspCol${gspDelta.gspId}`}>
               <div
-                className={
-                  selectedDeltaGsp === gspDelta.gspId ? selectedDeltaClass : selectedClasses
-                }
+                className={selectedDeltaGsp && clickedGspId === gspDelta.gspId ? selectedDeltaClass : selectedClasses}
                 key={`gspCol${gspDelta.gspId}`}
                 onClick={isSelectedGSP}
               >
-                <div className="flex pl-1 pt-1 w-32 items-center md:w-48">
+                <div className="flex pl-1 pt-3 w-48 md:w-48">
                   <span className="font-semibold text-lg">{gspDelta.gspRegion}</span>
-
-                  {/* <small>{gspDelta.gspId}</small> */}
                 </div>
-
-                <div className="flex flex-end justify-around pt-1">
-                  <div className="flex vertical-align-bottom font-semibold text-lg">
-                    {/* //deltavalue */}
-                    <div>
+                  
+                  <div className="flex flex-end justify-around items-center pt-1">
+                  
+                <DeltaForecastLabel
+                  tip={<div className="w-28  text-xs">
+                    <p>{"Normalized Delta"}</p>
+                  </div>}>
+                  
+                    <div className="flex pt-1.5 text-right opacity-80 text-sm w-16">
                       <p>
-                        {!negative && "+"}
-                        {Number(gspDelta.delta).toFixed(0)}
-                        <span className="opacity-80 text-xs font-thin">MW</span>
+                        {negative ? "-" : "+"}
+                        {deltaNormalizedPercentage}%
                       </p>
-                    </div>
-                  </div>
+                      </div>
+                </DeltaForecastLabel>
 
-                  {/* normalized percentage */}
-                  <div className="flex flex-col pt-1.5 pr-1 text-right opacity-80 text-sm md:w-16">
+                <DeltaForecastLabel
+                tip={<div className="w-28 text-xs">
+                    <p>{"Delta to Forecast"}</p>
+                  </div>}
+               >
+                <div className="flex pb-0 font-semibold text-lg md:w-20 w-16 justify-start">
+                  <div>
                     <p>
-                      {negative ? "-" : "+"}
-                      {deltaNormalizedPercentage}%
+                      {!negative && "+"}
+                      {Number(gspDelta.delta).toFixed(0)}
+                      <span className="opacity-80 text-xs font-thin">MW</span>
                     </p>
                   </div>
-
-                  {/* currentYield/forecasted yield */}
-                  <div className="flex flex-col pt-1.5 pr-1 text-right font-semibold text-sm md:w-32">
-                    <div>
-                      {Number(gspDelta.currentYield).toFixed(0)}/
-                      {Number(gspDelta.forecast).toFixed(0)}
-                      <span className={`opacity-80 text-xs font-thin`}>MW</span>
-                    </div>
                   </div>
+                  </DeltaForecastLabel>
+                {/* normalized percentage */}
+
+
+                {/* currentYield/forecasted yield */}
+                <DeltaForecastLabel
+                tip={<div className="w-32 text-xs">
+                    <p>{"Actual PV / Forecast"}</p>
+                  </div>}>
+                <div className="flex flex-col pt-1.5 pr-1 text-right font-semibold text-sm md:w-22 w-20">
+                  <div>
+                    {Number(gspDelta.currentYield).toFixed(0)}/
+                    {Number(gspDelta.forecast).toFixed(0)}
+                    <span className={`opacity-80 text-xs font-thin`}>MW</span>
+                  </div>
+                      </div>
+                      
+                    </DeltaForecastLabel>
+                  
                 </div>
-              </div>
-              <div
-                className={
-                  selectedDeltaGsp === gspDelta.gspId
-                    ? `bg-ocf-gray-800 ${
-                        gspDelta.delta > 0 ? `border-l-8` : `border-r-8`
-                      } ${bucketColor}`
-                    : `bg-ocf-delta-1000 ${
-                        gspDelta.delta > 0 ? `border-l-8` : `border-r-8`
-                      } ${bucketColor}`
-                }
-              >
-                <div
-                  className={`${
-                    gspDelta.delta > 0
-                      ? `flex flex-row-reverse items-end justify-end`
-                      : `flex items-end justify-end`
-                  }`}
+                    
+                  {/* <div
+                  className={`${gspDelta.delta > 0
+                      ? `static bottom-0 flex flex-row-reverse items-end justify-end`
+                      : `static bottom-0 flex items-end justify-end`}`}
                 >
                   <div className={tickerColor} style={{ width: `1px` }}></div>
                   <div
-                    className={`${progressLineColor} h-1`}
+                    className={`${selectedDeltaGsp === gspDelta.gspId ? `h-1.5` : `h-1`} ${progressLineColor}`}
                     style={{ width: `${deltaNormalizedPercentage}%` }}
-                  ></div>
+                          ></div>
+                          </div> */}
+          
                 </div>
-              </div>
-            </div>
+                <div className={selectedDeltaGsp === gspDelta.gspId
+                && clickedGspId === gspDelta.gspId ? ` bg-ocf-gray-800 ${gspDelta.delta > 0 ? `border-l-8` : `border-r-8`} ${bucketColor}`
+                : `${gspDelta.delta > 0 ? `border-l-8` : `border-r-8`} ${bucketColor}`}
+            >
+                <div
+                  className={`${gspDelta.delta > 0
+                      ? `bottom-0 flex flex-row-reverse items-end justify-end`
+                      : `flex items-end justify-end`}`}
+                >
+                  <div className={tickerColor} style={{ width: `1px` }}></div>
+                  <div
+                    className={`${selectedDeltaGsp === gspDelta.gspId ? `h-1.5` : `h-1`} ${progressLineColor}`}
+                    style={{ width: `${deltaNormalizedPercentage}%` }}
+                          ></div>
+                          </div>
+                      </div>
+            <div>
+                  </div>
+                </div>
+                </div>
+            </>
           );
         })}
       </div>
     </>
   );
-};
+}
+;
 
 const DeltaChart: FC<{ date?: string; className?: string }> = ({ className }) => {
   const [show4hView] = useGlobalState("show4hView");
---
+++ apps/nowcasting-app/components/delta-forecast-label.tsx
@@ -0,0 +1,34 @@
+type DeltaForecastLabelProps = {
+  tip: string | React.ReactNode;
+  position?: "left" | "right" | "middle";
+  className?: string;
+};
+
+const DeltaForecastLabel: React.FC<DeltaForecastLabelProps> = ({
+  children,
+  tip,
+  position = "left",
+  className
+}) => {
+  const getPositionClass = (position: "left" | "right" | "middle") => {
+    if (position === "left") return "-left-10";
+    if (position === "right") return "-right-10";
+    if (position === "middle") return "";
+  };
+  return (
+    <div className={`relative cursor-default flex group ${className || ""}`}>
+      {children}
+      <div className="absolute flex hidden mt-6 group-hover:flex flex-wrap">
+        <span
+          className={`relative ${getPositionClass(
+            position
+          )} flex top-1 text-center mb-0 mt-2z-10 text-xs px-1 py-1 leading-snug bg-ocf-gray-300 rounded-lg text-black`}
+        >
+          {tip}
+        </span>
+        </div>
+      </div>
+  );
+};
+
+export default DeltaForecastLabel;
