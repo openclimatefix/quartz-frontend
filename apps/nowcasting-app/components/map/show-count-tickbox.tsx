bf309716bfa476cb91afc3c138b0cd599b79f729
rachel-labri-tipton <rachemtipton@gmail.com>
2023-03-09 10:31:40 +0100

    fixing prettier error

--- apps/nowcasting-app/components/map/show-count-tickbox.tsx
+++ apps/nowcasting-app/components/map/show-count-tickbox.tsx
@@ -1,23 +1,28 @@
 import React from "react";
-import { useState } from "react"
+import { useState } from "react";
 import useGlobalState from "../helpers/globalState";
 
 const ShowSiteCount: React.FC<{ showCount?: boolean }> = () => {
-  const [showSiteCount, setShowSiteCount] = useGlobalState("showSiteCount")
+  const [showSiteCount, setShowSiteCount] = useGlobalState("showSiteCount");
   const toggleCount = () => {
-    setShowSiteCount(!showSiteCount)
-  }
+    setShowSiteCount(!showSiteCount);
+  };
   return (
     <div className="absolute top-20 right-10 right flex items-center mb-4">
-      <input onClick={toggleCount} type="checkbox" value="" className="ring-none border-2 bg-black text-black"></input>
+      <input
+        onClick={toggleCount}
+        type="checkbox"
+        value=""
+        className="ring-none border-2 bg-black text-black"
+      ></input>
       <label className="ml-2 text-base text-white font-medium">Show Site Count</label>
       {showSiteCount && (
         <div className="absolute top-14 right-10 right flex items-center mb-4 text-white">
-          Showing site count. 
+          Showing site count.
         </div>
       )}
-</div>
-  )
-}
+    </div>
+  );
+};
 
-export default ShowSiteCount
+export default ShowSiteCount;
