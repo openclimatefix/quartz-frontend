a2bbaad6c83ffe9b5a228c54db9d146aba362707
braddf <bradbdf@gmail.com>
2023-08-03 15:37:45 +0100

    Add api hook error retry func.

--- apps/nowcasting-app/components/hooks/useLoadDataFromApi.tsx
+++ apps/nowcasting-app/components/hooks/useLoadDataFromApi.tsx
@@ -34,6 +34,11 @@ export const useLoadDataFromApi = <T extends APIResponseType>(
   return useSWR<T, Error>(url, axiosFetcherAuth, {
     refreshInterval: t5min,
     dedupingInterval: t2min,
+    onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
+      if (error.toString().includes("403")) return;
+      if (retryCount >= 10) return;
+      setTimeout(() => revalidate({ retryCount }), 2000);
+    },
     ...config
   });
 };
