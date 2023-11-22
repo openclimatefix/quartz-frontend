ec5e3875c107cedaf13a574e943ccf358b35614b
Rachel Tipton <rachelmtipton@gmail.com>
2023-11-22 11:47:04 +0100

    try to get underline to work on deployment

--- apps/nowcasting-app/ChartInfo.tsx
+++ apps/nowcasting-app/ChartInfo.tsx
@@ -10,34 +10,23 @@ export const ChartInfo: React.FC = () => {
         <li>All datetimes are in Europe/London timezone.</li>
         <li>
           Following{" "}
-          <span className="font-bold underline">
-            <a
-              className="font-bold underline-blue"
-              href="https://www.solar.sheffield.ac.uk/pvlive/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              PV_Live
-            </a>
-          </span>
+          <span className="underline font-bold"><a
+            className="font-bold underline-blue"
+            href="https://www.solar.sheffield.ac.uk/pvlive/"
+            target="_blank"
+            rel="noreferrer"
+          >
+        PV_Live
+          </a></span>
           , datetimes show the end of the settlement period. <br />
           For example, 17:00 refers to solar generation between 16:30 to 17:00.
         </li>
         <li>The Y axis units are in MW for the National and GSP charts.</li>
-        <br />
-        <li>
-          To learn more, please see our{" "}
-          <span className="font-bold underline">
-            <a
-              href="https://openclimatefix.notion.site/How-to-use-the-Quartz-Solar-site-053005e5db144f6dbd7a3d0fe108d7f2"
-              target="_blank"
-              rel="noreferrer"
-            >
-              documentation
-            </a>
-          </span>
-          .
-        </li>
+        <br/>
+        <li>To learn more, please see our <span className="underline font-bold"><a
+        href="https://openclimatefix.notion.site/How-to-use-the-Quartz-Solar-site-053005e5db144f6dbd7a3d0fe108d7f2"
+            target="_blank"
+            rel="noreferrer">documentation</a></span>.</li>
       </ul>
     </div>
   );
