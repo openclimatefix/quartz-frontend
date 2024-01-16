4427753a3735fef666ac87167eab6a0f0ac2a573
Rachel Tipton <rachelmtipton@gmail.com>
2024-01-16 14:53:34 +0100

    save initial work on tests

--- apps/nowcasting-app/cypress/e2e/pvLatest.cy.js
+++ apps/nowcasting-app/cypress/e2e/pvLatest.cy.js
@@ -47,5 +47,50 @@ describe("Load the page", () => {
     cy.visit("http://localhost:3002/");
     cy.location("href").should("equal", "http://localhost:3002/");
     // TODO: Add tests for the PV Forecast page elements, probably with mocked data.
+    // national chart header
+    cy.get('[data-test="national-chart-header"]').contains("National").should("exist");
+    cy.get('[data-test="pv-ocf-forecast-headline-figure"]')
+      .contains("National")
+      .should("be.visible");
+    cy.get('[data-test="forecast-headline-figures"]').siblings().first().should("exist").click();
+    cy.get('[data-test="forecast-headline-figures"]').siblings().next().should("exist");
+    cy.get('[data-test="forecast-headline-figures"]').siblings().first().trigger("mouseover");
+    cy.get('[data-test="forecast-headline-figures"]')
+      .siblings()
+      .first()
+      .invoke("mouseover")
+      .should("contain", "PV Live / OCF Forecast");
+    cy.get('[data-test="forecast-headline-figures"]')
+      .siblings()
+      .first()
+      .trigger("mouseout")
+      .should("not.contain", "PV Live / OCF Forecast");
+    cy.get('[data-test="forecast-headline-figures"]')
+      .siblings()
+      .next()
+      .trigger("mouseover")
+      .contains("Next OCF Forecast");
+    cy.get('[data-test="forecast-headline-figures"]')
+      .siblings()
+      .next()
+      .trigger("mouseout")
+      .should("not.contain", "Next OCF Forecast");
+    // national chart play button
+    // play icon visible
+    // cy.get("data-test=national-chart-play-button").should("exist", "be.visible");
+    // // pause icon not visible
+    // // play icon visible
+    // cy.get("data-test=national-chart-play-button").should("exist", "be.visible").click();
+    // cy.get("data-test=national-chart-play-button").should("exist", "be.visible").click();
+
+    // national chart
+    // gsp chart header
+    // gsp chart
+    // gsp chart close button
+    // national pv chart legend check that elements are there
+    // legend select and deselect lines and check that they disappear and reappear
+    // national map with date and time
+    // national map with color scale
+    // national map buttons for capacity and generation
   });
 });
