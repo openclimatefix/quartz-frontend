1c02d4b744f271c90ea54780b1f3ae4b8d72149d
peterdudfield <peter.dudfield@hotmail.com>
2024-01-24 19:15:11 +0000

    add function description and cases

--- apps/nowcasting-app/components/map/sitesMap.tsx
+++ apps/nowcasting-app/components/map/sitesMap.tsx
@@ -101,7 +101,23 @@ const SitesMap: React.FC<SitesMapProps> = ({
   ];
 
   const getRingMultiplier = (aggregationLevel: AGGREGATION_LEVELS, maxCapacity: number) => {
-    return 4000 / maxCapacity;
+
+    // Get the circle multiplier based, which will times the Expected PV value to get the radius
+    // We used to use 0.3 for National, 1.5 for DNO, 5 for GSP and 10 for Sites.
+    // Now we use a multiplier based on the max capacity of the the different sites/regions.
+    // If a client has 100 3 KW sites. Then the National multipler will be about 0.3.
+
+    switch (aggregationLevel) {
+      case AGGREGATION_LEVELS.SITE:
+        return 30 / maxCapacity;
+      case AGGREGATION_LEVELS.GSP:
+        return 45 / maxCapacity;
+      case AGGREGATION_LEVELS.REGION:
+        return 60 / maxCapacity;
+      case AGGREGATION_LEVELS.NATIONAL:
+        return 100 / maxCapacity;
+    }
+
   };
 
   const generateGeoJsonForecastData: (
