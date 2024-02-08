f406f34ffe05493224e8c570b5f2e7e5cae033f0
braddf <bradbdf@gmail.com>
2024-02-08 16:07:01 +0000

    new nextjs app initial commit

---
+++ apps/quartz-app/src/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+type SidebarProps = {};
+
+const Sidebar: React.FC<SidebarProps> = () => {
+  return (
+    <div className="flex-0 w-96 justify-center items-center flex bg-ocf-gray-700">
+      <h1>Sidebar</h1>
+    </div>
+  );
+};
+
+export default Sidebar;
