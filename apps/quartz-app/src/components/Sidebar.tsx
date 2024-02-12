27dd36759d827f16d398f4bb160d3dc9a24218d2
braddf <bradbdf@gmail.com>
2024-02-12 15:24:30 +0000

    add OCF greys from design files; fix config

--- apps/quartz-app/src/components/Sidebar.tsx
+++ apps/quartz-app/src/components/Sidebar.tsx
@@ -2,7 +2,7 @@ type SidebarProps = {};
 
 const Sidebar: React.FC<SidebarProps> = () => {
   return (
-    <div className="flex-0 w-96 justify-center items-center flex bg-ocf-gray-700">
+    <div className="flex-0 w-96 justify-center items-center flex bg-ocf-grey-700">
       <h1>Sidebar</h1>
     </div>
   );
