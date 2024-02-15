b64894f99fbc7700b83f7f8bee58ad885e656149
braddf <bradbdf@gmail.com>
2024-02-15 18:32:30 +0000

    update api to proxied quartz route

--- apps/quartz-app/src/data/apiClient.ts
+++ apps/quartz-app/src/data/apiClient.ts
@@ -2,8 +2,7 @@ import createClient from "openapi-fetch";
 import { paths } from "@/src/types/schema";
 
 const client = createClient<paths>({
-  baseUrl:
-    "http://india-development-india-api.ap-south-1.elasticbeanstalk.com/",
+  baseUrl: "https://api-dev.quartz.energy/",
   // "http://localhost:8000/",
   // headers: {
   //   Authorization: `Bearer ${process.env.API_KEY}`,
