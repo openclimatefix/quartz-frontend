574948b09020154b9872c6b8bbf06d7e8dcf893a
Rachel Tipton <rachelmtipton@gmail.com>
2024-02-19 09:59:17 +0100

    sidebar with card and minicard components

--- apps/quartz-app/src/components/sidebar-components/card.tsx
+++ apps/quartz-app/src/components/sidebar-components/card.tsx
@@ -6,12 +6,13 @@ interface CardProps {
   actualGeneration: number;
   currentForecast?: number;
   nextForecast?: number;
-  theme: string;
+  bgTheme: string;
+  textTheme: string;
   largeText?: string;
   smallText?: string;
 }
 
-const WideCard: React.FC<CardProps> = ({icon, actualGeneration, currentForecast, nextForecast, energyTag, theme, largeText, smallText}) => {
+const WideCard: React.FC<CardProps> = ({icon, actualGeneration, currentForecast, nextForecast, energyTag, bgTheme, textTheme}) => {
   return (
     // add clock component here and pass the time as props
     <div className="self-stretch h-[136px] flex-col justify-start items-start gap-2 flex">
@@ -21,13 +22,13 @@ const WideCard: React.FC<CardProps> = ({icon, actualGeneration, currentForecast,
                     <div className="w-8 h-8 relative">{icon}</div>
           <div className="w-[60px] text-white text-base font-medium font-sans uppercase">{energyTag}</div>
           <div className="w-6 h-3 relative">
-                      <div className={`w-6 h-1 left-0 top-[4px] absolute bg-${theme} rounded-2xl`}></div>
+                      <div className={`w-6 h-1 left-0 top-[4px] absolute ${bgTheme} rounded-2xl`}></div>
                     </div>
                   </div>
                 </div>
                 <div className="self-stretch justify-between items-start inline-flex">
-                  <div className={`w-[105px] text-${theme} text-5xl font-bold font-sans leading-[64px]`}>{currentForecast}</div>
-                  <div className={`text-${theme} text-5xl font-bold font-sans leading-[64px]`}>{nextForecast}</div>
+                  <div className={`w-[105px] ${textTheme} text-5xl font-bold font-sans leading-[64px]`}>{currentForecast}</div>
+                  <div className={`${textTheme} text-5xl font-bold font-sans leading-[64px]`}>{nextForecast}</div>
                 </div>
               </div>
               
