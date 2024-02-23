b3af1dffa8b31395fc2dc2c1ebcbdedaed21e7eb
braddf <bradbdf@gmail.com>
2024-02-23 12:07:54 +0000

    Bugfix for map flashing issue 🤞🏻
    (cherry picked from commit 56b4d57577c490aeb5d6277fdd4b1c7a2e7e57f4)

--- apps/nowcasting-app/components/helpers/mapUtils.ts
+++ apps/nowcasting-app/components/helpers/mapUtils.ts
@@ -12,10 +12,26 @@ export const safelyUpdateMapData = (
     !map.isStyleLoaded()
   ) {
     if (!map.isStyleLoaded()) {
-      setTimeout(() => {
-        // console.log("map is not style loaded, trying again");
+      // Check if we've already set a timeout for this map
+      const existingTimeout = localStorage.getItem(
+        `MapTimeoutId-${map.getContainer().dataset.title}`
+      );
+      // If we have, clear it
+      if (existingTimeout) {
+        // console.log(`clearing existing map timeout for ${map.getContainer().dataset.title}`);
+        clearTimeout(Number(existingTimeout));
+      }
+      // Set a new timeout to check if the map is ready and update the data
+      // console.log(`setting new map timeout for ${map.getContainer().dataset.title}`, newTimeout);
+      const newTimeout = setTimeout(() => {
+        // console.warn("map is not style loaded, trying again");
         safelyUpdateMapData(map, updateMapData);
-      }, 400);
+      }, 500);
+      // Save the timeout id to local storage
+      localStorage.setItem(
+        `MapTimeoutId-${map.getContainer().dataset.title}`,
+        newTimeout.toString()
+      );
     }
     return;
   } else {
