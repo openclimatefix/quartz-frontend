11e6bed436d8158516fe8a992607c755c6ec0800
Rachel Tipton <rachelmtipton@gmail.com>
2024-02-26 11:28:15 +0100

    pushing work for pair code session

--- apps/quartz-app/src/components/sidebar-components/mini-card.tsx
+++ apps/quartz-app/src/components/sidebar-components/mini-card.tsx
@@ -41,24 +41,7 @@ const MiniCard: React.FC<MiniCardProps> = ({
       </div>
     </div>
   );
-  {
-    /* //  <div className="self-stretch h-[84px] flex-col justify-start items-start gap-4 flex">
-    //   <div className="self-stretch h-10 flex-col justify-start items-start gap-2 flex">
-    //           <div className="self-stretch justify-center items-center gap-2 inline-flex">
-    //            {icon}
-    //       <div className="w-4 h-4 relative"></div>
-    //     </div>
-    //     <div className="self-stretch text-center text-white text-base font-bold font-sans leading-none">{actualGeneration}</div>
-    //     <div className="w-6 h-3 relative">
-    //         <div className={`w-6 h-1 left-0 top-[4px] mt-5 absolute bg-${theme} rounded-2xl`}></div>
-    //       </div>
-    //   </div>
-    //   <div className="self-stretch h-7 flex-col justify-start items-center gap-1 flex">
-    //     <div className="h-2 px-2 py-4 justify-center items-center gap-2 inline-flex"><RightArrow/></div>
-    //     <div className={`text-center text-${theme} text-base font-bold font-sans leading-none mb-2"`}>{nextForecast}</div>
-    //   </div>
-    //         </div> */
-  }
+  
 };
 
 export default MiniCard;
