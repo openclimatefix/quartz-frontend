003972882f11de0b3e819aaac77f48a972dfe046
Rachel Tipton <rachelmtipton@gmail.com>
2024-02-26 11:28:15 +0100

    pushing work for pair code session

--- apps/quartz-app/src/components/Sidebar.tsx
+++ apps/quartz-app/src/components/Sidebar.tsx
@@ -19,234 +19,137 @@ import { get } from "http";
 
 type SidebarProps = {
   title: string;
-  windForecastData:
-    | components["schemas"]["GetForecastGenerationResponse"]
-    | undefined;
-  windGenerationData:
+    solarGenerationData:
     | components["schemas"]["GetHistoricGenerationResponse"]
     | undefined;
-  solarGenertaionData:
+  windGenerationData:
     | components["schemas"]["GetHistoricGenerationResponse"]
     | undefined;
   solarForecastData:
     | components["schemas"]["GetForecastGenerationResponse"]
     | undefined;
+  windForecastData:
+    | components["schemas"]["GetForecastGenerationResponse"]
+    | undefined;
 };
 
-const data = {
-  actualPowerGeneration: 5.8,
-  currentPowerForecast: 6.7,
-  nextPowerForecast: 4.2,
-  actualWindGeneration: 5.4,
-  currentWindForecast: 5.6,
-  nextWindForecast: 2.1,
-  actualSolarGeneration: 0.4,
-  currentSolarForecast: 0.9,
-  nextSolarForecast: 2.1,
-};
+// const data = {
+//   actualPowerGeneration: 5.8,
+//   currentPowerForecast: 6.7,
+//   nextPowerForecast: 4.2,
+//   actualWindGeneration: 5.4,
+//   currentWindForecast: 5.6,
+//   nextWindForecast: 2.1,
+//   actualSolarGeneration: 0.4,
+//   currentSolarForecast: 0.9,
+//   nextSolarForecast: 2.1,
+// };
 
 const Sidebar: React.FC<SidebarProps> = ({
   windForecastData,
   windGenerationData,
   solarForecastData,
-  solarGenertaionData,
+  solarGenerationData,
   title,
 }) => {
-  // const { data, error } = useGetRegionsQuery("solar");
-  // console.log("Sidebar data test", data);
+  // function to convert the datestamp to epoch
 
   const convertDatestampToEpoch = (time: string) => {
-    const date = new Date(time);
+    const date = new Date(time.slice(0, 16));
     return date.getTime();
   };
 
   const formatDate = (time: number) => {
     const date = new Date(time);
-    return date.toLocaleString();
+    date.toLocaleString();
   };
+
 
   const getNowInTimezone = () => {
     const now = DateTime.now().setZone("ist");
-    console.log("now", now);
-    const dateInTimezone = DateTime.fromISO(now.toString().slice(0, 16)).set({
+    return DateTime.fromISO(now.toString().slice(0, 16)).set({
       hour: now.minute >= 45 ? now.hour + 1 : now.hour,
-      minute: now.minute < 45 ? Math.floor(now.minute / 15) : 0,
+      minute: now.minute < 45 ? Math.floor(now.minute / 15) * 15 : 0,
       second: 0,
       millisecond: 0,
     });
-    return dateInTimezone.toMillis();
   };
-  console.log("getNowInTimezone", getNowInTimezone());
 
-  // get timestamp in IST
-  const getTimestampInTimezone = (timestamp: number) => {
-    const date = DateTime.now().setZone("ist");
-    console.log("date", date);
-    const dateInTimezone = DateTime.fromISO(date.toString().slice(0, 16)).set({
-      hour: date.minute >= 45 ? date.hour + 1 : date.hour,
-      minute: date.minute < 45 ? Math.floor(date.minute / 15) : 0,
-      second: 0,
-      millisecond: 0,
-    });
-    return dateInTimezone.toMillis();
+  const getNext15MinSlot = () => {
+    const now = getNowInTimezone();
+    if (now.minute >= 45) {
+      return now.plus({ hours: 1 }).set({ minute: 0 });
+    } else if (now.minute < 45) {
+      return now.set({ minute: ((Math.floor(now.minute / 15) * 15) + 15) });
+    }
+  }
+
+  const getEpochNowInTimezone = () => {
+    return getNowInTimezone().toMillis();
   };
-  console.log("getTimestampInTimezone", getTimestampInTimezone(1633926600000));
 
-  // functions to
+  // function to get the current time as 00:00 format
+  const prettyPrintNowTime = () => {
+    return getNowInTimezone().toFormat("HH:mm");
+  };
+
+  // function to get the next 15 min slot as 00:00 format
+  const prettyPrintNextTime = () => {
+    return getNext15MinSlot().toFormat("HH:mm");
+  }
+
 
   const formattedSidebarData: {
     timestamp: number;
     solar_generation?: number;
     wind_generation?: number;
-    solar_forecast?: number;
-    wind_forecast?: number;
+    solar_forecast_past?: number;
+    solar_forecast_future?: number;
+    wind_forecast_past?: number;
+    wind_forecast_future?: number;
   }[] = [];
 
-  //   // Loop through wind forecast and add to formattedSolarData
-  // if (windForecastData?.values) {
-  //   for (const value of windForecastData?.values) {
-  //     const timestamp = convertDatestampToEpoch(value.Time);
-  //     const existingData = formattedChartData?.find(
-  //       (data) => data.timestamp === timestamp
-  //     );
-  //     if (existingData) {
-  //       existingData.wind_forecast = value.PowerKW / 1000;
-  //     } else {
-  //       formattedChartData?.push({
-  //         timestamp,
-  //         wind_forecast: value.PowerKW / 1000,
-  //       });
-  //     }
-  //   }
-  // }
-  // Loop through solar forecast and add to formattedSolarData
-  //get the latest solar generation data and the timestamp
-  // const latestSolarGeneration = windGenerationData?.values[0].PowerKW / 1000 || 0;
-  // latestSolarGeneration = solarGenerationData?.values[0].PowerKW / 1000 || 0;
-  // const latestSolarGenerationTimestamp = convertDatestampToEpoch(solarGenerationData?.values[0].Time || "");
-
+  // Loop through wind generation and add to formattedSidebarData
   if (windForecastData?.values) {
     for (const value of windForecastData?.values) {
       const timestamp = convertDatestampToEpoch(value.Time);
       const existingData = formattedSidebarData?.find(
         (data) => data.timestamp === timestamp
       );
+      const key =
+        timestamp < getEpochNowInTimezone()
+          ? "wind_forecast_past"
+          : "wind_forecast_future";
       if (existingData) {
-        existingData.wind_forecast = value.PowerKW / 1000;
+        existingData[key] = value.PowerKW / 1000;
       } else {
         formattedSidebarData?.push({
           timestamp,
-          wind_forecast: value.PowerKW / 1000,
+          [key]: value.PowerKW / 1000,
         });
       }
     }
   }
+  
 
-  // Loop through solar generation and add to formattedSolarData
+  // // Loop through solar generation and add to formattedSolarData
   if (windGenerationData?.values) {
     for (const value of windGenerationData?.values) {
       const timestamp = convertDatestampToEpoch(value.Time);
       const existingData = formattedSidebarData?.find(
         (data) => data.timestamp === timestamp
       );
-      if (existingData && existingData.wind_forecast) {
-        existingData.wind_generation = value.PowerKW / 1000;
-      }
-    }
-  }
-  // Loop through solar forecast and add to formattedSolarData
-  if (solarForecastData?.values) {
-    for (const value of solarForecastData?.values) {
-      const timestamp = convertDatestampToEpoch(value.Time);
-      const existingData = formattedSidebarData?.find(
-        (data) => data.timestamp === timestamp
-      );
       if (existingData) {
-        existingData.solar_forecast = value.PowerKW / 1000;
+        existingData.wind_generation = value.PowerKW / 1000;
       } else {
         formattedSidebarData?.push({
           timestamp,
-          solar_forecast: value.PowerKW / 1000,
+          wind_generation: value.PowerKW / 1000,
         });
       }
     }
   }
-  // Loop through solar generation and add to formattedSolarData
-  if (solarGenertaionData?.values) {
-    for (const value of solarGenertaionData?.values) {
-      const timestamp = convertDatestampToEpoch(value.Time);
-      const existingData = formattedSidebarData?.find(
-        (data) => data.timestamp === timestamp
-      );
-      if (
-        existingData &&
-        (existingData.solar_forecast || existingData.wind_forecast)
-      ) {
-        existingData.solar_generation = value.PowerKW / 1000;
-      }
-    }
-  }
-  const formattedGenerationData = formattedSidebarData.sort(
-    (a, b) => b.timestamp - a.timestamp
-  );
-
-  console.log(formatDate(formattedGenerationData[2]?.timestamp));
-  console.log(getNowInTimezone());
-
-  let latestActualWindGeneration =
-    formattedGenerationData[0]?.wind_generation?.toFixed(2) || 0;
-  latestActualWindGeneration = Number(
-    Number(latestActualWindGeneration) / 1000
-  ).toFixed(2);
-
-  let currentWindForecast =
-    formattedGenerationData[0]?.wind_forecast?.toFixed(2) || 0;
-  currentWindForecast = Number(Number(currentWindForecast) / 1000).toFixed(2);
-  let nextWindForecast =
-    formattedGenerationData[1]?.wind_forecast?.toFixed(2) || 0;
-  nextWindForecast = Number(Number(nextWindForecast) / 1000).toFixed(2);
-
-  let actualSolarGeneration =
-    formattedGenerationData[0]?.solar_generation?.toFixed(2) || 0;
-  actualSolarGeneration = Number(Number(actualSolarGeneration) / 1000).toFixed(
-    2
-  );
-  let currentSolarForecast =
-    formattedGenerationData[0]?.solar_forecast?.toFixed(2) || 0;
-  currentSolarForecast = Number(Number(currentSolarForecast) / 1000).toFixed(2);
-  let nextSolarForecast =
-    formattedGenerationData[1]?.solar_forecast?.toFixed(2) || 0;
-  nextSolarForecast = Number(Number(nextSolarForecast) / 1000).toFixed(2);
-
-  const actualPowerGeneration =
-    Number(latestActualWindGeneration + 0).toFixed(2) || 0;
-  const currentPowerForecast = Number(currentWindForecast + 0).toFixed(2) || 0;
-  const nextPowerForecast = Number(nextWindForecast + 0).toFixed(2) || 0;
-
-  console.log("latestActualWindGeneration", latestActualWindGeneration);
-
-  console.log("timestamps", formattedGenerationData);
-
-  console.log("sidebarformattedGenerationData", formattedSidebarData);
-
-  console.log("sidebarformattedGenerationData", formattedSidebarData);
-  // if (solarGenerationData?.values) {
-  //   for (const value of solarGenerationData?.values) {
-  //     const timestamp = convertDatestampToEpoch(value.Time);
-  //     const existingData = formattedSidebarData?.find(
-  //       (data) => data.timestamp === timestamp
-  //     );
-  //     if (existingData &&
-  //   existingData.solar_forecast ) {
-  //       existingData.solar_generation = value.PowerKW / 1000;
-  //     } else {
-  //       formattedSidebarData?.push({
-  //         timestamp,
-  //         solar_generation: value.PowerKW / 1000,
-  //       });
-  //     }
-  //   }
-  // }
+  // // Loop through solar forecast and add to formattedSolarData
   // if (solarForecastData?.values) {
   //   for (const value of solarForecastData?.values) {
   //     const timestamp = convertDatestampToEpoch(value.Time);
@@ -263,6 +166,107 @@ const Sidebar: React.FC<SidebarProps> = ({
   //     }
   //   }
   // }
+  // // Loop through solar generation and add to formattedSolarData
+  // if (solarGenerationData?.values) {
+  //   for (const value of solarGenerationData?.values) {
+  //     const timestamp = convertDatestampToEpoch(value.Time);
+  //     const existingData = formattedSidebarData?.find(
+  //       (data) => data.timestamp === timestamp
+  //     );
+  //     if (
+  //       existingData &&
+  //       (existingData.solar_forecast || existingData.wind_forecast)
+  //     ) {
+  //       existingData.solar_generation = value.PowerKW / 1000;
+  //     }
+  //   }
+  // }
+  const formattedGenerationData = formattedSidebarData.sort(
+    (a, b) => b.timestamp - a.timestamp
+  );
+
+  
+  let actualWindGeneration =
+    formattedGenerationData[0]?.wind_generation?.toFixed(2) || 0;
+  actualWindGeneration = Number(
+    Number(actualWindGeneration) / 1000
+  ).toFixed(2);
+
+  let currentWindForecast =
+    formattedGenerationData[0]?.wind_forecast.toFixed(2) || 0;
+  currentWindForecast = Number(Number(currentWindForecast) / 1000).toFixed(2);
+  let nextWindForecast =
+    formattedGenerationData[1]?.wind_forecast?.toFixed(2) || 0;
+  nextWindForecast = Number(Number(nextWindForecast) / 1000).toFixed(2);
+
+  let actualSolarGeneration =
+    formattedGenerationData[0]?.solar_generation?.toFixed(2) || 0;
+  actualSolarGeneration = Number(Number(actualSolarGeneration) / 1000).toFixed(
+    2
+  );
+
+  let currentSolarForecast =
+    formattedGenerationData[0]?.solar_forecast?.toFixed(2) || 0;
+  currentSolarForecast = Number(Number(currentSolarForecast) / 1000).toFixed(2);
+
+  let nextSolarForecast =
+    formattedGenerationData[1]?.solar_forecast?.toFixed(2) || 0;
+  nextSolarForecast = Number(Number(nextSolarForecast) / 1000).toFixed(2);
+
+  const actualPowerGeneration =
+    Number(actualWindGeneration+ 0).toFixed(2) || 0;
+  const currentPowerForecast = Number(currentWindForecast + 0).toFixed(2) || 0;
+  const nextPowerForecast = Number(nextWindForecast + 0).toFixed(2) || 0;
+
+  console.log("latestActualWindGeneration", actualSolarGeneration);
+
+  console.log("sidebarformattedGenerationData", formattedSidebarData);
+
+  console.log(
+    "formattedSidebarData",
+    formattedSidebarData.map((d) => ({
+      prettyPrint: new Date(d.timestamp).toLocaleString(),
+      ...d,
+    }))
+  );
+
+
+  console.log("nextDate", prettyPrintNextTime());
+  console.log("nowDate", prettyPrintNowTime());
+
+  if (solarGenerationData?.values) {
+    for (const value of solarGenerationData?.values) {
+      const timestamp = convertDatestampToEpoch(value.Time);
+      const existingData = formattedSidebarData?.find(
+        (data) => data.timestamp === timestamp
+      );
+      if (existingData &&
+    existingData.solar_forecast ) {
+        existingData.solar_generation = value.PowerKW / 1000;
+      } else {
+        formattedSidebarData?.push({
+          timestamp,
+          solar_generation: value.PowerKW / 1000,
+        });
+      }
+    }
+  }
+  if (solarForecastData?.values) {
+    for (const value of solarForecastData?.values) {
+      const timestamp = convertDatestampToEpoch(value.Time);
+      const existingData = formattedSidebarData?.find(
+        (data) => data.timestamp === timestamp
+      );
+      if (existingData) {
+        existingData.solar_forecast = value.PowerKW / 1000;
+      } else {
+        formattedSidebarData?.push({
+          timestamp,
+          solar_forecast: value.PowerKW / 1000,
+        });
+      }
+    }
+  }
 
   console.log("sidebarformattedGenerationData", formattedSidebarData);
 
@@ -299,19 +303,19 @@ const Sidebar: React.FC<SidebarProps> = ({
               <div className="w-[350px] h-px border border-white border-opacity-40"></div>
               <div className="self-stretch h-[39px] justify-start items-start gap-4 inline-flex">
                 <ForecastTimeDisplay
-                  time={5}
+                  time={prettyPrintNowTime()}
                   icon={<ClockIcon />}
                   forecastTag="NOW GW"
                 />
                 <ForecastTimeDisplay
-                  time={5}
+                  time={prettyPrintNextTime()}
                   icon={<ClockIcon />}
                   forecastTag="NEXT GW"
                 />
               </div>
               <WideCard
                 icon={<WindIcon />}
-                actualGeneration={latestActualWindGeneration.toString()}
+                actualGeneration={actualWindGeneration.toString()}
                 currentForecast={currentWindForecast}
                 nextForecast={nextWindForecast}
                 energyTag="Wind"
@@ -358,7 +362,7 @@ const Sidebar: React.FC<SidebarProps> = ({
               icon={<WindIcon />}
               textTheme={"text-quartz-energy-200"}
               bgTheme={"bg-quartz-energy-200"}
-              actualGeneration={latestActualWindGeneration}
+              actualGeneration={actualWindGeneration}
               nextForecast={nextWindForecast}
             />
             <div className="w-full h-px mt-4 border border-white border-opacity-40"></div>
