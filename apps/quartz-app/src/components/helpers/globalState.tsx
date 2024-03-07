853948553ab4cf575f533867c88940eb1496761e
braddf <bradbdf@gmail.com>
2024-03-07 10:52:41 +0000

    Specify createGlobalState return type explicitly

--- apps/quartz-app/src/components/helpers/globalState.tsx
+++ apps/quartz-app/src/components/helpers/globalState.tsx
@@ -6,9 +6,36 @@ export type GlobalStateType = {
   visibleLines: string[];
 };
 
-export const { useGlobalState, getGlobalState, setGlobalState } =
-  createGlobalState<GlobalStateType>({
-    visibleLines: ["Solar", "Wind"],
-  });
+export const {
+  useGlobalState,
+  getGlobalState,
+  setGlobalState,
+}: Pick<
+  {
+    useGlobalStateProvider: () => void;
+    useGlobalState: <StateKey extends keyof GlobalStateType>(
+      stateKey: StateKey
+    ) => readonly [
+      GlobalStateType[StateKey],
+      (u: import("react").SetStateAction<GlobalStateType[StateKey]>) => void
+    ];
+    getGlobalState: <StateKey_1 extends keyof GlobalStateType>(
+      stateKey: StateKey_1
+    ) => GlobalStateType[StateKey_1];
+    setGlobalState: <StateKey_2 extends keyof GlobalStateType>(
+      stateKey: StateKey_2,
+      update: import("react").SetStateAction<GlobalStateType[StateKey_2]>
+    ) => void;
+    getState: () => GlobalStateType;
+    setState: (nextGlobalState: GlobalStateType) => void;
+    dispatch: (action: never) => never;
+  },
+  | "useGlobalStateProvider"
+  | "useGlobalState"
+  | "getGlobalState"
+  | "setGlobalState"
+> = createGlobalState<GlobalStateType>({
+  visibleLines: ["Solar", "Wind"],
+});
 
 export default useGlobalState;
