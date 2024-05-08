56b996c3d15f10047101b59737462172daf3da8b
braddf <bradbdf@gmail.com>
2024-05-08 16:48:06 +0100

    Add env var for api url

--- apps/quartz-app/src/data/apiClient.ts
+++ apps/quartz-app/src/data/apiClient.ts
@@ -2,7 +2,7 @@ import createClient from "openapi-fetch";
 import { paths } from "@/src/types/schema";
 
 const client = createClient<paths>({
-  baseUrl: "https://api-dev.quartz.energy/",
+  baseUrl: process.env.NEXT_PUBLIC_API_URL || "https://api-dev.quartz.energy/",
   // "http://localhost:8000/",
   // headers: {
   //   Authorization: `Bearer ${process.env.API_KEY}`,
